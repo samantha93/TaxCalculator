34b66b85cd9515a28e1d75c6ca755d3c1f0631ee
Florent LE BOULCH <florent.leboulch@ceetiz.com>
2019-12-19 16:57:05 +0100

    Add failing unit tests

--- src/test/java/com/sak/ceetiz/bean/SASCompanyTest.java
+++ src/test/java/com/sak/ceetiz/bean/SASCompanyTest.java
@@ -1,15 +1,36 @@
 package com.sak.ceetiz.bean;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static junit.framework.Assert.assertEquals;
 
 public class SASCompanyTest {
+
+    private static final String SIRET_NUMBER = "Siret number";
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void testCalculateTax_PositiveCapitalForSasCompany_ShouldReturnTax() {
         Company sas = new SAS(150, "Siret number", "name", "address");
         double actual = sas.calculateTax();
         assertEquals(49.5, actual);
+    }
+
+    @Test
+    public void two_sas_with_same_siret_should_be_equal() {
+        AbstractCompany sas = new SAS(150, SIRET_NUMBER, "name 1", "address 1");
+        AbstractCompany sas2 = new SAS(333, SIRET_NUMBER, "name 2", "address 2");
+
+        assertEquals(sas, sas2);
+    }
+
+    @Test
+    public void create_a_sas_with_null_siret_is_forbidden() {
+        expectedException.expect(IllegalArgumentException.class);
+        AbstractCompany sas = new SAS(150, null, "name 1", "address 1");
     }
 }
